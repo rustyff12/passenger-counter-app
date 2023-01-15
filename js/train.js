71ec13b91d9624e0929968b6503e63fb13f7ad86
Russell Frankpitt <75209707+rustyff12@users.noreply.github.com>
2023-01-15 23:47:02 +0100

    increment-btn + js added

--- js/train.js
+++ js/train.js
@@ -1,1 +1,13 @@
-document.getElementById("count-el").innerText = 5;
+let countEl = document.getElementById("count-el")
+let count = 0;
+
+function increment() {
+    count += 1;
+    countEl.innerText = count;
+}
+
+
+function save() {
+    console.log(count);
+}
+
